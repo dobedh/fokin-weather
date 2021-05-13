7e1a767225734d197d4f71fb9bd4ec501e687b13
dobedh <dobedh@gmail.com>
2021-05-14 00:46:01 +0900

    state, location get permission request

--- App.js
+++ App.js
@@ -1,17 +1,32 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
+import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
 
 export default class extends React.Component {
+  state = {
+    isLoading: true,
+  };
   getLocation = async () => {
-    const location = await Location.getCurrentPositionAsync();
-    console.log(this.location);
+    try {
+      await Location.requestForegroundPermissionsAsync();
+      const {
+        coords: { altitude, latitude },
+      } = await Location.getCurrentPositionAsync();
+      console.log(altitude, latitude);
+      this.setState({
+        isLoading: false,
+      });
+    } catch (error) {
+      Alert.alret("Sorry", "it's sad to say");
+    }
   };
   componentDidMount() {
     return this.getLocation();
   }
   render() {
-    return <Loading />;
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
   }
 }
