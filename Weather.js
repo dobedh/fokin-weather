26b595c2cb51fcfc6ae92c87e754a6e6e67ce8f5
dobedh <dobedh@gmail.com>
2021-05-16 22:23:48 +0900

    expo linear gradient

--- Weather.js
+++ Weather.js
@@ -1,23 +1,87 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, StatusBar } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+const weatherOptions = {
+  Haze: {
+    iconName: "weather-hail",
+    gradient: ["red", "blue"],
+  },
+  Rain: {
+    iconName: "weather-rainy",
+    gradient: ["#4c669f", "#3b5998", "#192f6a"],
+  },
+  Thunderstorm: {
+    iconName: "",
+    gradient: [],
+  },
+  Drizzle: {
+    iconName: "",
+    gradient: [],
+  },
+
+  Snow: {
+    iconName: "",
+    gradient: [],
+  },
+  Atmosphere: {
+    iconName: "",
+    gradient: [],
+  },
+  Clear: {
+    iconName: "",
+    gradient: [],
+  },
+  Clouds: {
+    iconName: "",
+    gradient: [],
+  },
+  Mist: {
+    iconName: "",
+    gradient: [],
+  },
+  Dust: {
+    iconName: "",
+    gradient: [],
+  },
+};
 
 export default function Weather({ temp, description }) {
   return (
-    <View style={styles.container}>
+    <LinearGradient
+      colors={weatherOptions[description].gradient}
+      style={styles.container}
+    >
       <View style={styles.halfContainer}>
-        <MaterialCommunityIcons name="weather-rainy" size={100} color="blue" />
+        <MaterialCommunityIcons
+          name={weatherOptions[description].iconName}
+          size={100}
+          color="white"
+        />
         <Text style={styles.text}>{temp}°C</Text>
       </View>
       <View style={styles.halfContainer} />
-    </View>
+    </LinearGradient>
   );
 }
 
 Weather.propTypes = {
   temp: PropTypes.number.isRequired,
-  description: PropTypes.string.isRequired,
+  description: PropTypes.oneOf([
+    "Thunderstorm",
+    "Drizzle",
+    "Rain",
+    "Snow",
+    "Atmosphere",
+    "Clear",
+    "Clouds",
+    "Haze",
+    "Mist",
+    "Dust",
+    "Haze",
+  ]).isRequired,
 };
 
 const styles = StyleSheet.create({
@@ -33,5 +97,6 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 32,
+    color: "white",
   },
 });
